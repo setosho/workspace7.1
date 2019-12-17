47a37cfbd843fc5e69a8349e90bdad92e2437b4d
瀬戸翔太郎 <mutton_omomuki@setoshoutarounoMacBook-Pro.local>
2019-12-18 00:33:18 +0900

    検索機能更新

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ApplicationRecord
   validates :task_name, presence: true
   validates :detail, presence: true
-  scope :search, -> (task_name, task_status) {where('(task_name LIKE ?) AND (status LIKE ?)',"%#{task_name}%", "%#{task_status}%")}
+  scope :search, -> (params) {where('(task_name LIKE ?) AND (status LIKE ?)',
+                                 "%#{params[:task][:task_name_key]}%",
+                                 "%#{params[:task][:status_key]}%")}
   enum priority: {not_entered: 0, low: 1, medium: 2, high: 3}
 end
